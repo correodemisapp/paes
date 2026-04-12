e8b67f5f6d78804537b52dfcca8cf32878ccf930
correodemisapp <correodemisapps@gmail.com>
2026-04-11 23:37:44 -0400

    Add Firebase configuration and Firestore initialization

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // ← añadir esto
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,8 +9,9 @@ const firebaseConfig = {
   projectId: "paes-app-82aab",
   storageBucket: "paes-app-82aab.firebasestorage.app",
   messagingSenderId: "72270848497",
-  appId: "1:72270848497:web:85b5ef3a02a89599b3a450"
+  appId: "1:72270848497:web:85b5ef3a02a89599b3a450",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app); // ← añadir esto
